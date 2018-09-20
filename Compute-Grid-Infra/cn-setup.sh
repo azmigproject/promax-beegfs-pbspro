cdd4921f817b10d37647ce1469edaa88613cf1e0
azmigproject <azmigproject@outlook.com>
2018-09-20 22:55:26 +0530

    Updated to update nsswitch.conf

--- Compute-Grid-Infra/cn-setup.sh
+++ Compute-Grid-Infra/cn-setup.sh
@@ -140,7 +140,11 @@ EOF
     #sed -i 's/required_domain="mydomain.local"/required_domain="nxad01.pttep.local"/g' /etc/dhcp/dhclient-exit-hooks.d/azure-cloud.sh
     chmod 755 /etc/dhcp/dhclient-exit-hooks
     echo "in set_DNS, updated Execute permission for dhclient-exit-hooks"
-
+	sed -i  "s/hosts:      files/hosts:      files nis/g"  /etc/nsswitch.conf
+	sed -i  "s/passwd:     files/passwd:     files nis/g"  /etc/nsswitch.conf
+	sed -i  "s/shadow:     files/shadow:     files nis/g"  /etc/nsswitch.conf
+	sed -i  "s/group:      files/group:      files nis/g"  /etc/nsswitch.conf
+	
 	#sed -i  "s/networks:   files/networks:   files nis [NOTFOUND=return]/g"  /etc/nsswitch.conf
 	#sed -i  "s/hosts:      files dns/hosts: files dns [NOTFOUND=return]/g"  /etc/nsswitch.conf
     echo "in set_DNS, updated nsswitch resolv.conf, restarting network service"
