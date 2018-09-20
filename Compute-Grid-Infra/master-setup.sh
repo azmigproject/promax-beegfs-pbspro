e7bac3171b9912c43420c44d8c3f96e6c3267bbf
azmigproject <azmigproject@outlook.com>
2018-09-20 17:52:40 +0530

    Added logic to setup NIS server and create mapping for ip ranges

--- Compute-Grid-Infra/master-setup.sh
+++ Compute-Grid-Infra/master-setup.sh
@@ -4,6 +4,7 @@
 log()
 {
 	echo "$1"
+	ADDRESS_SPACE=$1
 }
 
 while getopts :a:k:u:t:p optname; do
@@ -16,6 +17,9 @@ while getopts :a:k:u:t:p optname; do
     k)  # storage key
 		export AZURE_STORAGE_ACCESS_KEY=${OPTARG}
 		;;
+	j)  # storage key
+	export ADDRESS_SPACE_LIST=${OPTARG}
+	;;
   esac
 done
 
@@ -105,7 +109,67 @@ mount_nfs()
 	service nfs start
 		
 }
-
+create_nismap()
+{		
+	IFS=',' read -ra ADDR <<< "$ADDRESS_SPACE_LIST"
+        for i in "${ADDR[@]}"; do
+			 Suffix="$(cut -d'/' -f2 <<<$i)"
+			 IP="$(cut -d'/' -f1 <<<$i)"
+			 digit1="$(cut -d. -f1 <<<$IP)"
+			 digit2="$(cut -d. -f2 <<<$IP)"
+			 digit3="$(cut -d. -f3 <<<$IP)"
+			 digit4="$(cut -d. -f4 <<<$IP)"
+			 BIT=`expr 32 - $Suffix`
+			 value=1
+			for j in `seq $BIT`
+			do
+			        value=$(( $value * 2 ))
+			done
+			for l in `seq $value`
+			do
+			    echo "host-$digit1-$digit2-$digit3-$digit4  $digit1.$digit2.$digit3.$digit4" >>mymap.temp
+			    digit4=$((digit4=digit4+1))
+			    rem=$(($l % 256))
+			    if [ $rem == 0 ]; then
+			            digit3=$((digit3=digit3+1))
+			            digit4=0
+			    fi
+			done
+        done
+}
+nis_server()
+{
+	
+	SERVER_IP="$(ip addr show eth0 | grep 'inet ' | cut -f2 | awk '{ print $2}')"
+    hostip="$(echo ${SERVER_IP} | sed 's\/.*\\g')"	
+	yum -y install ypserv rpcbind ypbind
+	ypdomainname nishub.local
+	echo "NISDOMAIN=nishub.local" >> /etc/sysconfig/network
+	echo "$hostip main.nishub.local main" >> /etc/hosts
+	echo "domain nishub.local server main.nishub.local" >> /etc/yp.conf
+	/etc/init.d/network restart
+	/etc/init.d/rpcbind start	
+	/etc/init.d/ypserv  start
+	/etc/init.d/ypxfrd  start
+	/etc/init.d/yppasswdd start	
+	chkconfig rpcbind on 
+	chkconfig ypserv on 
+	chkconfig ypxfrd on 
+	chkconfig yppasswdd on
+	chkconfig ypbind on	
+	grep | /usr/lib64/yp/ypinit -m
+	sleep 10
+	/etc/init.d/ypbind start
+	#/etc/init.d/rpcbind restart
+	#/etc/init.d/ypbind restart	  
+	cd /var/yp
+	/usr/lib64/yp/makedbm -u nishub.local/hosts.byname> mymap.temp
+	create_nismap
+	/usr/lib64/yp/makedbm mymap.temp nishub.local/hosts.byname
+	yppush hosts.byname	
+	make
+}
+nis_server
 mount_nfs_suse()
 {
 	log "install NFS"
